423e8141a378bb853900e6fc29b1df23c610de12
Euler G Gonçalves <goncalves.euler@gmail.com>
2021-10-13 10:15:19 -0300

    versão 2
    criação e impressão de tabuleiro
    preenchimento com espaço
    entrada de player1

--- pingponggame.java
+++ pingponggame.java
@@ -1,30 +1,52 @@
+import java.util.Scanner;
+
+
+
+
 public class pingponggame {
 
-        public static void imprimir() {
+	public static void imprimir(String tabuleiro[][], int m, int n) {
 
-                System.out.println("==========================================================");
-                for (int i=0; i<=10; i++) {
-                        System.out.print("=");
-                        for (int j=0; j<=55; j++) {
-                                System.out.print(" ");
+		System.out.println("==========================================================");
+		for (int i=0; i<m; i++) {
+			System.out.print("=");
+			for (int j=0; j<n; j++) {
+				System.out.print(tabuleiro[i][j]);
 
-                        }
-                        System.out.println("=");
-                }
-                System.out.println("==========================================================");
-                System.out.println("\n");
+			}
+			System.out.println("="); 
+		}
+		System.out.println("==========================================================");
+		System.out.println("\n");
+
+		
+	}
 
 
-        }
+	public static void main(String[] args) {
+		Scanner player1 = new Scanner(System.in);
+		System.out.print("Digite seu nome:");
+		String idade = player1.nextLine();
+		System.out.println("");
 
 
-        public static void main(String[] args) {
-                while (true) {
 
-                        try { Thread.sleep(500); } catch(InterruptedException ex) {}
+		int m = 10;
+		int n = 55;
+		String[][] tabuleiro = new String[m][n];
+		for (int i=0; i<m; i++) {
+			for (int j=0; j<n; j++) {
+				tabuleiro[i][j] = " ";
+			}
+		}
 
-                        imprimir();
 
-                }
-        }
+		while (true) {
+
+			try { Thread.sleep(500); } catch(InterruptedException ex) {}
+
+			imprimir(tabuleiro, m, n);
+
+		}
+	}
 }
